4085ee08923df0a458b75918b462bf03075304d8
Sushant BK <sushantbk@mail.weber.edu>
2025-02-10 11:10:08 -0700

    Add:input for plkayer name

--- game.py
+++ game.py
@@ -1,11 +1,19 @@
-''''''
+'''
 Adventure Game
 Author: Sushant BK
 Version: 1.0
 Description:
 This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
+'''
+
 
 # Welcome message and Introduction
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...") 
+
+# Ask for the player's name
+player_name = input("what is your name, adventurer?")
+
+# Concatenate strings to create a personalized messafe
+print("Welcome, " +player_name + 'Your journey begins now.' ) 
