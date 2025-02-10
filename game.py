a078a5134f209dd6a118dc7b4495dbf5454a59c6
Sushant BK <sushantbk@mail.weber.edu>
2025-02-10 10:50:58 -0700

    ADD Docstring for program info

--- game.py
+++ game.py
@@ -1,2 +1,11 @@
+''''''
+Adventure Game
+Author: Sushant BK
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+
+# Welcome message and Introduction
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...") 
